d69565e9db5ef901c74416e24dbe622510b1d26f
nonokaM <c1201071@gmail.com>
2023-07-16 22:01:03 +0900

    feat: タスク追加機能

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Hello World'
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
